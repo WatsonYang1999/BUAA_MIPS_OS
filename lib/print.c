7ffda771785f21373e782456cfd7553a898a6b6d
杨华晟 <17373344@buaa.edu.cn>
2019-03-25 11:03:09 +0800

    1
    1

--- lib/print.c
+++ lib/print.c
@@ -272,13 +272,17 @@ PrintChar(char * buf, char c, int length, int ladjust)
 {
     int i;
     
-    if (length < 1) length = 1;
+    if (length < 1) 
+		length = 1;
     if (ladjust) {
-	*buf = c;
-	for (i=1; i< length; i++) buf[i] = ' ';
-    } else {
-	for (i=0; i< length-1; i++) buf[i] = ' ';
-	buf[length - 1] = c;
+		*buf = c;
+		for (i=1; i< length; i++)
+			buf[i] = ' ';
+    } 
+	else {
+		for (i=0; i< length-1; i++) 
+			buf[i] = ' ';
+		buf[length - 1] = c;
     }
     return length;
 }
